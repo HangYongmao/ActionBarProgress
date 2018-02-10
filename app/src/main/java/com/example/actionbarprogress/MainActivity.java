20a77cce9d6b045aec6cd76f2a4bdf46f8bae95b
HangYongmao <1981217256@qq.com>
2018-02-10 22:59:21 +0800

    ProgressDialog显示

--- app/src/main/java/com/example/actionbarprogress/MainActivity.java
+++ app/src/main/java/com/example/actionbarprogress/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.actionbarprogress;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,6 +18,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button reduce;
     private Button reset;
     private TextView text;
+
+    private ProgressDialog progressDialog;
+    private Button show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +45,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         reduce = (Button) findViewById(R.id.reduce);
         reset = (Button) findViewById(R.id.reset);
         text = (TextView) findViewById(R.id.text);
+        show = (Button) findViewById(R.id.show);
+
         refresh();
         add.setOnClickListener(this);
         reset.setOnClickListener(this);
         reduce.setOnClickListener(this);
+        show.setOnClickListener(this);
     }
 
     @Override
@@ -64,6 +73,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 progressBar.setProgress(50);
                 progressBar.setSecondaryProgress(80);
                 break;
+
+            case R.id.show:
+                // 新建ProgressDialog对象
+                progressDialog = new ProgressDialog(MainActivity.this);
+                // 设置显示风格
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                // 设置对话框标题
+                progressDialog.setTitle("慕课网");
+                // 设置对话框的文字信息
+                progressDialog.setMessage("欢迎大家支持慕课网");
+                // 设置图标
+                progressDialog.setIcon(R.mipmap.ic_launcher);
+
+                // 设定最大进度
+                progressDialog.setMax(100);
+                // 设定初始化进度
+                progressDialog.incrementProgressBy(50);
+                // 进度条是明确显示进度的
+                progressDialog.setIndeterminate(false);
+
+                // 设定一个确定按钮
+                progressDialog.setButton(DialogInterface.BUTTON_POSITIVE, "确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "欢迎大家支持慕课网", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // 是否可以通过返回按钮退出对话框
+                progressDialog.setCancelable(true);
+
+                // 显示ProgressDialog
+                progressDialog.show();
+
+                return;
 
             default:
                 break;
